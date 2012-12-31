e8cec4c3e739611569f3423728adaadbe2eee584
Yusuf Abdulla Shunan <shunan@gmail.com>
2012-12-31 21:08:45 +0500

    wysihtml5 defaults in script.js

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -9,7 +9,15 @@
 // 	} );
 
 $(document).ready(function() {
-	$('.textarea').wysihtml5();
+	$('.textarea').wysihtml5({
+		"font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+		"emphasis": true, //Italics, bold, etc. Default true
+		"lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+		"html": false, //Button which allows you to edit the generated HTML. Default false
+		"link": true, //Button to insert a link. Default true
+		"image": true, //Button to insert an image. Default true,
+		"color": false //Button to change color of font
+	});
 	$('.datepicker').datepicker({
 				format: 'yyyy-mm-dd'
 			});
